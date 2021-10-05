3fa5c9eca7f76838d5711b8a2edac9cb7eb31142
Andrea Vargas Castro <16310731@itlp.edu.mx>
2021-10-05 17:38:25 -0500

    [FIXED]: Method accessibility

--- src/share.js
+++ src/share.js
@@ -1,4 +1,13 @@
 function showOptios() {
-  var element = document.getElementById("image-footer");
+  let element = document.getElementById("image-footer");
   element.classList.toggle('active');
+
+  let isActive = element.classList.contains('active')
+
+  let btn = document.querySelector("button");
+  btn.setAttribute("aria-expanded", isActive);
+
+  let profile = document.querySelector(".profile");
+  profile.setAttribute("aria-hidden", isActive);
+  profile.setAttribute("alt", isActive ? "" : "michelle appleton");
 }
